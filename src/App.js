fb805b2a1930aaf649303d51a049d755aa1d9c58
Eric Rohner <ericrohner22@gmail.com>
2018-08-21 22:03:52 -0600

    The assassin is ready

--- src/App.js
+++ src/App.js
@@ -7,49 +7,62 @@ class App extends Component {
     this.state = { names: [], name: '', victims: [] }
   }
 
-  handleSubmit(e) {
+  addVictim(e) {
     e.preventDefault()
-    this.state.names.push(this.state.name)
-    this.setState({ name: '' })
-    console.log(this.state.names)
+    if (this.state.name !== '') {
+      this.state.names.push(this.state.name)
+      this.setState({ name: '' })
+      console.log(this.state.names)
+    }
   }
 
-  kill(e) {
-    //e.preventDefault()
-    const dead = Math.floor(Math.random() * this.state.names.length)
-    // this.state.victims.push(this.state.names[dead])
-    this.setState(oldState => {
-      return {
-        victims: [...oldState.victims, this.state.names[dead] ]
-      }
-    })
-    console.log(this.state.victims)
+  kill() {
+    if (this.state.names.length > 0) {
+      const dead = this.state.names[Math.floor(Math.random() * this.state.names.length)]
+      const weapons = ['\u2693', '\u2694', '\u26CF', '\u26BE', '\u2622', '\u2623', '\u{1F346}', '\u2604', '\u231B',
+        '\u261D', '\u{1F30B}', '\u{1F3F9}', '\u{1F409}', '\u{1F494}', '\u{1f525}', '\u{1F489}', '\u{1F982}', '\u{1F991}']
+      const weapon = weapons[Math.floor(Math.random() * weapons.length)]
+      this.setState(oldState => {
+        return {
+          victims: [weapon + ' ' + dead, ...oldState.victims],
+          names: oldState.names.filter((name, index, names) => {
+            return name !== dead
+          })
+        }
+      })
+    }
   }
+
+
 
   render() {
     return (
-      <div className="App">
-        <form onSubmit={(e) => this.handleSubmit(e)}>
+      <div className = 'App'>
+        <h1>Hello, I'm your personal assassin!</h1>
+        <p>Be careful! Once you give the order to kill I cannot be stopped.</p>
+        <p>I will make sure one of your enemies meets their doom and</p>
+        <p>report back with their cause of death. I do my work very quickly.</p>
+        <form onSubmit={(e) => this.addVictim(e)}>
           <input
             value={this.state.name}
             onChange={event => this.setState({ name: event.target.value })}
             type="text"
-            placeholder="Add a victim!"
+            placeholder="Victim's name"
           />
           <button type="submit">
-            Add victim to list
+            Add victim
+          </button>
+          <button type="button" onClick={() => this.kill()}>
+            Kill!
           </button>
         </form>
-        <h1>Hit List:</h1>
+        <h2>Hit List:</h2>
         {this.state.names.map((name, index, names) => {
           return <li>{name}</li>
         })}
-        <button type="submit" onClick={(e) => this.kill(e)}>
-          Kill!
-        </button>
         <h2>Dead:</h2>
         {this.state.victims.map((victim, index, victims) => {
-          return <li>{victim}</li>
+          return <div>{victim}</div>
         })}
 
 
